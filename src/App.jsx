c253614223ba733406ded0b0647c937a1e0e6ca4
a-hagi613 <abdullahi.hagi98@gmail.com>
2023-08-19 23:28:37 -0400

    removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -30,31 +30,3 @@ function App() {
 }
 
 export default App;
-
-// import logo from "./logo.svg";
-// import "./App.css";
-// import React from "react";
-// import { useEffect, useState } from "react";
-
-// function App() {
-//   const [todos, setTodos] = useState([]);
-
-//   useEffect(() => {
-//     fetch("https://jsonplaceholder.typicode.com/todos")
-//       .then(response => response.json())
-//       .then(json => setTodos(json));
-//   }, []);
-
-//   return (
-//     <>
-//       {todos.map(todo => (
-//         <div key={todo.id}>
-//           <h1>{todo.title}</h1>
-//           <p>{todo.completed}</p>
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
-
-// export default App;
