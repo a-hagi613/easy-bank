87595054bc832e7652cc6d99be7fe6918e61a2b6
a-hagi613 <abdullahi.hagi98@gmail.com>
2023-08-21 00:28:29 -0400

    testing

--- src/components/Heros.jsx
+++ src/components/Heros.jsx
@@ -5,28 +5,28 @@ import imgMock from "../assets/images/image-mockups.png";
 
 export default function Heros() {
   return (
-    <Row >
+    <Row>
       <Col lg={5} className="ms-5">
         <br />
         <br />
-        <br />  <br />
+        <br /> <br />
         <br />
         <br />
         <br />
-        
-       
-        <h1  className="px-5">Next Generation Digital Banking</h1>
-        <p className="px-5 mt-3 text-secondary"> 
+        <h1 className="px-5">Next Generation Digital Lanking</h1>
+        <p className="px-5 mt-3 text-secondary">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
           libero nesciunt aut consequatur delectus fugit nobis fuga, natus
           necessitatibus ea officiis recusandae aliquam, sint incidunt cumque
           eius veritatis illum provident?
           <br />
           <br />
-          <button className="gradient buttons2 p-auto mt-2">Request Invite</button>
+          <button className="gradient buttons2 p-auto mt-2">
+            Request Invite
+          </button>
         </p>
       </Col>
-      <Col  className="ms-5 hero-bg">
+      <Col className="ms-5 hero-bg">
         <img src={imgMock} />
       </Col>
     </Row>
